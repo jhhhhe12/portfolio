3c005563b8f18f8839e1d19159cf19057c2cb541
jhhhhe <jhhhhe@example.com>
2025-12-24 00:32:11 +0000

    Clean up: Remove all duplicate root files, keep only Portfolio/ directory

--- docker/nodejs_test.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var content = function(req, resp){
-  resp.writeHead(200);
-  resp.end("Good morning Korea~!\n");
-}
-var web = http.createServer(content);
-web.listen(8002);
-console.log('Server running at http://localhost:8002/');
